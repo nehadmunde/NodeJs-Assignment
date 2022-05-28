dbd174ba8f2941e113f77fc9e74ee8ae2fc03603
Neha <nehadmunde@gmail.com>
2022-05-29 02:03:11 +0530

    Express Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [state,setState]= useState([]);
+  const [value,setValue]=useState({
+    name:'',
+    password:''
+});
+   const [flag,setFlag]=useState(false);
+  const handleChange=(event)=>{
+    setValue({...value,[event.target.name]:event.target.value});
+    console.log('value from handleChange',value)
+  }
+
+  const handleSubmit=()=>{
+    axios.post("http://localhost:9000/post",value).then(res=>{
+      console.log('value from axios',value);
+      alert('Data is Saved.');
+      setFlag(!flag)
+    }).catch(err=>{
+      console.log(err);
+    })
+    
+  }
+  useEffect(()=>{
+    
+    axios.get("http://localhost:9000").then(res=>{
+       setState(res.data);
+       console.log('setState from useEffect',state);
+    }).catch(err=>{
+      console.log(err);
+    })
+  },[flag])
+    return (
+    <div className="App col-md-9 offset-2">
+
+      <h1>Basic Express App</h1>
+     
+      <br/>
+      
+      <input type='text' name='name' placeholder='Enter User Name Here' className='form-control' onChange={handleChange} />
+      <br/>
+      <input type='password' name='password' placeholder='Enter Password Here' className='form-control' onChange={handleChange} />
+      <br/>
+      <button className='btn btn-info' onClick={handleSubmit}>Submit</button>
+    
+    <h4>Data from Server </h4>
+    <hr/>
+      <table className="table table-striped">
+  <thead>
+    <tr>
+    <th scope="col">ID</th>
+      <th scope="col">User Name</th>
+      <th scope="col">Password</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+     state.map((ele,id)=>(
+
+    <tr key={id} >
+        <td>{id+1}</td>
+      <td>{ele.name}</td>
+      <td>{ele.password}</td>
+
+    </tr>
+     ))
+  }
+  </tbody>
+</table>
+      
+
+    
     </div>
   );
 }
